e6bfcc1b8e5825d7e71ea97b4bd525a4e5a24a31
Gopichandanpani <shaktipani124@gmail.com>
2026-02-26 23:17:40 +0530

    update

--- backend/hrms_backend/settings.py
+++ backend/hrms_backend/settings.py
@@ -87,8 +87,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+CSRF_TRUSTED_ORIGINS = [
+    "https://hrms-fullstack-eblo.onrender.com"
 ]
 
 STATIC_URL = "/static/"
